352a508620df2db91a5fa49dcabef11e709e7b8f
ayla kim <ayla_12@naver.com>
2023-08-14 20:05:20 +0900

    패키지 설치

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './App.css';
+import { BrowserView, MobileView } from 'react-device-detect'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const App = () => {
+  return (
+    <div className="App">
+      <BrowserView>
+        데스크톱브라우져!
+      </BrowserView>
+      <MobileView>
+        모바일 브라우져!
+      </MobileView>
+      
+    </div>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export default App;
