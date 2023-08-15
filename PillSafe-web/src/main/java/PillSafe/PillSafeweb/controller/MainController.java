57ca60dba6a2a0c3cfa2e85c2c7642d4e6e2afca
박지은 <31093793+lucykorea414@users.noreply.github.com>
2023-08-15 20:17:53 +0900

    Update MainController.java
    delete: unused import

--- PillSafe-web/src/main/java/PillSafe/PillSafeweb/controller/MainController.java
+++ PillSafe-web/src/main/java/PillSafe/PillSafeweb/controller/MainController.java
@@ -1,11 +1,7 @@
 package PillSafe.PillSafeweb.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class MainController {
