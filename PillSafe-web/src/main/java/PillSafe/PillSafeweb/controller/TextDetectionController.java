ff0e39b37b331a4e3fb6ac82213829db65817e9d
박지은 <31093793+lucykorea414@users.noreply.github.com>
2023-08-16 18:21:57 +0900

    main controller 삭제, TextDetectionController 수정

--- PillSafe-web/src/main/java/PillSafe/PillSafeweb/controller/TextDetectionController.java
+++ PillSafe-web/src/main/java/PillSafe/PillSafeweb/controller/TextDetectionController.java
@@ -8,13 +8,24 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
+import org.springframework.web.util.UriUtils;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
 
 @Controller
 public class TextDetectionController {
 
-    @GetMapping("/")
+    @GetMapping("/home")
+    public String home(){
+        return "home";
+    }
+
+    @GetMapping("/upload")
     public String showUploadForm() {
         return "upload";
     }
@@ -41,21 +52,56 @@ public class TextDetectionController {
         String apiUrl = "http://apis.data.go.kr/1471000/DrbEasyDrugInfoService/getDrbEasyDrugList";
         String serviceKey = "qAKbXRR4042vBsi3b39VoNv8bKlELiGAo046m1w5E3%2FifqQqoz%2B%2Fp9cel5cGeKAtD0HhA9RDU65b8NIGvd4DqQ%3D%3D";
 
+        // Decode the URL-encoded textResult parameter
+        String decodedTextResult = UriUtils.decode(textResult, "UTF-8");
+
         // Make the API call and fetch data
-        String apiResponse = makeApiCall(apiUrl, serviceKey, textResult);
+        String apiResponse = makeApiCall(apiUrl, serviceKey, decodedTextResult);
 
         model.addAttribute("apiResponse", apiResponse);
         return "apiResult";
     }
 
-    private String makeApiCall(String apiUrl, String serviceKey, String itemName) {
-        RestTemplate restTemplate = new RestTemplate();
+//    private String makeApiCall(String apiUrl, String serviceKey, String itemName) {
+//        RestTemplate restTemplate = new RestTemplate();
+//
+//        // Construct the API request URL with required parameters
+//        UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(apiUrl)
+//                .queryParam("serviceKey", serviceKey)
+//                .queryParam("itemName", itemName);
+//
+//
+//        System.out.println(serviceKey);
+//        System.out.println(builder.build(false).toUriString());
+//
+////        builder.build(false).toUri();
+////        URI uri = new URI(builder.build(false).toUri());
+////        String jsonString = restTemplate.getForObject(uri, String.class);
+//
+//        return restTemplate.getForObject(builder.build(false).toUri(), String.class);
+//    }
 
-        // Construct the API request URL with required parameters
-        UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(apiUrl)
-                .queryParam("serviceKey", serviceKey)
-                .queryParam("itemName", itemName);
+    private String makeApiCall(String apiUrl, String serviceKey, String itemName){
+        StringBuffer result = new StringBuffer();
+        try{
+            String urlstr = apiUrl + "?ServiceKey=" + serviceKey + "&itemName=" + itemName;
 
-        return restTemplate.getForObject(builder.toUriString(), String.class);
+            URL url = new URL(urlstr);
+            HttpURLConnection urlconnection = (HttpURLConnection) url.openConnection();
+            urlconnection.setRequestMethod("GET");
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(urlconnection.getInputStream(), "UTF-8"));
+
+            String returnLine;
+            result.append("<xmp>");
+            while((returnLine = br.readLine()) != null){
+                result.append(returnLine + "\n");
+            }
+            urlconnection.disconnect();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        return result+"</xmp>";
     }
 }
