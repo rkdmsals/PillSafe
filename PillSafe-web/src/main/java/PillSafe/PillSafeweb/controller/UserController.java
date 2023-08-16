d4e06fd3cba7b2b253678e8aa50827fbb974560d
박지은 <31093793+lucykorea414@users.noreply.github.com>
2023-08-17 04:42:39 +0900

    login html 연결

---
+++ PillSafe-web/src/main/java/PillSafe/PillSafeweb/controller/UserController.java
@@ -0,0 +1,12 @@
+package PillSafe.PillSafeweb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+}
