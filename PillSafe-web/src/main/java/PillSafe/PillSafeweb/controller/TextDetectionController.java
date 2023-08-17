0c855204ae8c93fb34d2d4a1c4b566b86550d87c
박지은 <31093793+lucykorea414@users.noreply.github.com>
2023-08-17 21:49:41 +0900

    보안 이슈

--- PillSafe-web/src/main/java/PillSafe/PillSafeweb/controller/TextDetectionController.java
+++ PillSafe-web/src/main/java/PillSafe/PillSafeweb/controller/TextDetectionController.java
@@ -98,8 +98,6 @@ public class TextDetectionController {
 
 
 
-
-
     @GetMapping("/getDrugInfo")
     public String getDrugInfo(@RequestParam("textResult") String textResult, Model model) {
         String apiUrl = "http://apis.data.go.kr/1471000/DrbEasyDrugInfoService/getDrbEasyDrugList";
@@ -111,9 +109,6 @@ public class TextDetectionController {
         // Make the API call and fetch data
         String apiResponse = makeApiCall(apiUrl, serviceKey, textResult);
 
-//       model.addAttribute("apiResponse", apiResponse);
-
-        Parse(apiResponse);  //파싱 로그: 터미널로 확인용
         jsonResult(apiResponse, model);
 
         return "apiResult"; // 리다이렉트 요청
@@ -130,9 +125,6 @@ public class TextDetectionController {
         // Make the API call and fetch data
         String apiResponse = makeApiCall(apiUrl, serviceKey, textResult);
 
-//       model.addAttribute("apiResponse", apiResponse);
-
-        Parse(apiResponse);  //파싱 로그: 터미널로 확인용
         jsonResult(apiResponse, model);
 
         return "searchResult"; // 리다이렉트 요청
@@ -153,57 +145,18 @@ public class TextDetectionController {
         model.addAttribute("items", items);
     }
 
-    private void Parse(String result){
-        try {
-            JSONParser jsonParser = new JSONParser();
-            JSONObject object = (JSONObject)jsonParser.parse(result);
-
-            JSONObject parse_object = (JSONObject) object.get("body");
-            JSONArray array = (JSONArray) parse_object.get("items");
-
-            for(int i=0; i<array.size(); i++) {
-                System.out.println("항목[" + (i + 1) + "] ===========================================");
-
-                //배열 안에 있는것도 JSON형식 이기 때문에 JSON Object 로 추출
-                JSONObject tmp = (JSONObject) array.get(i);
-
-                //JSON name으로 추출
-                System.out.println("업체명 ==> " + tmp.get("entpName"));
-                System.out.println("제품명 ==> " + tmp.get("itemName"));
-                System.out.println("효능 ==> " + tmp.get("efcyQesitm"));
-                System.out.println("사용법 ==> " + tmp.get("useMethodQesitm"));
-                System.out.println("주의사항 경고 ==> " + tmp.get("atpnWarnQesitm"));
-                System.out.println("이미지 경로 ==>" + tmp.get("itemImage"));
-            }
-
-        } catch (ParseException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-
-
     private String makeApiCall(String apiUrl, String serviceKey, String itemName){
-//        StringBuffer result = new StringBuffer();
         StringBuilder result = new StringBuilder();
         try{
             String encodedItemName = URLEncoder.encode(itemName, StandardCharsets.UTF_8); // URL 인코딩 적용
 
             String urlstr = apiUrl + "?ServiceKey=" + serviceKey + "&itemName=" + encodedItemName + "&type=json";
-//            String urlstr = apiUrl + "?ServiceKey=" + serviceKey + "&itemName=" + itemName;
 
             URL url = new URL(urlstr);
             HttpURLConnection urlconnection = (HttpURLConnection) url.openConnection();
             urlconnection.setRequestMethod("GET");
 
             BufferedReader br = new BufferedReader(new InputStreamReader(urlconnection.getInputStream(), StandardCharsets.UTF_8));
-
-            // StringBuffer 일때
-//            String returnLine;
-//            while((returnLine = br.readLine()) != null){
-//                result.append(returnLine);
-//            }
-//            urlconnection.disconnect();
 
             // String Builder 일때
             String returnLine;
